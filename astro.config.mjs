d0f82c32268afad23d607c5b8bfc4e4acf5568f9
DrxcoDev <drxcovalorant@gmail.com>
2024-07-20 18:27:01 +0200

    Commiy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
   output: "server",
   adapter: netlify()
 });
